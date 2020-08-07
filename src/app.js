10e60091a6162e27c14e040d05cc887c1bfaa489
Sébastien Vallet <sebastien.vallet89@gmail.com>
2020-08-07 02:04:28 +0200

    added log save

--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.set('trust proxy', true);
 
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 
 app.get('/ping', (req, res) => {
   const clientIpAddress = req.ip;
-  res.send(200, `${clientIpAddress}`);
+
+  const logLine = `[${new Date().toISOString()}] - ${clientIpAddress}\n`;
+
+  fs.open('var/log/ip-list.log', 'a', (err, fd) => {
+    if (err) throw err;
+    fs.appendFile(fd, logLine, 'utf8', (err) => {
+      fs.close(fd, (err) => {
+        if (err) throw err;
+      });
+      if (err) throw err;
+    });
+  });
+
+  res.status(200).send(clientIpAddress);
 });
 
 // '0.0.0.0' force the ip address to be ipv4 to avoid ::ffff: prefix
